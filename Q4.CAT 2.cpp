471f2b3dce874f55887036f7c0d100ab932c0826
Michael Joseph <michael.ngala@strathmore.edu>
2024-07-18 08:22:35 +0300

    Add files via upload

---
+++ Q4.CAT 2.cpp
@@ -0,0 +1,34 @@
+//Q4.
+#include <iostream>//Preprocessor directive
+using namespace std;//names and variables library
+int main() {//Execution function
+//Variable declaration
+    int num1, num2;
+
+    // Asking user for input
+    cout << "Enter the first number: \n";
+    cin >> num1;
+    cout << "Enter the second number: \n";
+    cin >> num2;
+
+    // Calculate difference between num1 and num2
+    int diff = num1 - num2;
+
+    switch(diff > 0) {
+        case true:
+            cout << "The maximum number is: " << num1 << "\n";
+            break;
+        case false:
+            switch(diff < 0) {
+                case true:
+                    cout << "The maximum number is: " << num2 <<"\n";
+                    break;
+                case false:
+                    cout << "Both numbers are equal." << "\n";
+                    break;
+            }
+            break;
+    }
+
+    return 0;
+}//end of execution
